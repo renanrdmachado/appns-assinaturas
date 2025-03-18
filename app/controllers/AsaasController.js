fea7240ca71bf5663d50abf80fa3b644b16da317
m6rc0sp <paulo.marcos0108@live.com>
2025-03-18 16:54:22 -0300

    Remove rotas da API Asaas, centralizando a lógica de gerenciamento em um único arquivo

--- app/controllers/AsaasController.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const asaasControllers = require('./asaas');
-
-// Exportando todas as funções dos controladores modulares
-module.exports = {
-    ...asaasControllers
-};
--- app/services/AsaasService.js
+++
@@ -1,2 +0,0 @@
-// Este arquivo foi removido. 
-// Importe os serviços específicos dos módulos em app/services/asaas/ diretamente.
